588eb52c5df55319e05e8f1f9674bbc39638e44c
Shayokh <shayokhshuvro14@gmail.com>
2025-05-22 18:02:29 +0600

    add todo arranger functionality

--- main.ts
+++ main.ts
@@ -1,130 +1,76 @@
-import { type App, type Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { type Editor, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CheckboxReorderPlugin extends Plugin {
+	private isProcessing = false;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(
+			this.app.workspace.on("editor-change", (editor: Editor) => {
+				this.processEditor(editor);
+			})
+		);
 	}
 
-	onunload() {
+	async processEditor(editor: Editor) {
+		if (this.isProcessing) return;
 
-	}
+		const content = editor.getValue();
+		const lines = content.split("\n");
+		const checkedLines: string[] = [];
+		const uncheckedLines: string[] = [];
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+		// Separate checked and unchecked checkboxes
+		for (const line of lines) {
+			const trimmed = line.trim();
+			if (trimmed.startsWith("- [x]") || trimmed.startsWith("- [X]")) {
+				checkedLines.push(line);
+			} else if (trimmed.startsWith("- [ ]")) {
+				uncheckedLines.push(line);
+			}
+		}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+		// Skip if no checkboxes found
+		if (checkedLines.length === 0) return;
+
+		// Combine checked first, then unchecked
+		const newContent = [...checkedLines, ...uncheckedLines].join("\n");
+
+		// Update editor content if changed
+		if (newContent !== content) {
+			this.isProcessing = true;
+			editor.setValue(newContent);
+			this.isProcessing = false;
+		}
 	}
 }
 
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
+// import { Plugin } from 'obsidian';
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
+// export default class TodoPlugin extends Plugin {
+// 	statusBarTextElement: HTMLElement;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// 	async onload() {
+// 		this.statusBarTextElement = this.addStatusBarItem().createEl('span')
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+// 		this.app.workspace.on('active-leaf-change', async () => {
+// 			const file = this.app.workspace.getActiveFile();
 
-	display(): void {
-		const { containerEl } = this;
+// 			if (file) {
+// 				const content = await this.app.vault.read(file);
+// 				this.updateLineCount(content);
+// 			} else { this.updateLineCount("0"); }
+// 		})
 
-		containerEl.empty();
+// 		this.app.workspace.on('editor-change', (editor) => {
+// 			const content = editor.getDoc().getValue();
+// 			this.updateLineCount(content);
+// 		})
+// 	}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+// 	updateLineCount(fileContent?: string) {
+// 		if (fileContent) {
+// 			this.statusBarTextElement.textContent = fileContent === "0" ?
+// 				"0 lines" : `${fileContent.split('\n').length} lines`;
+// 		}
+// 	}
+// }
